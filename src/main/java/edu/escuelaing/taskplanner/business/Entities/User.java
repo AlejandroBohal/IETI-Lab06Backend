80a4dec14b4f20bf1b6acd2f08384623af142371
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2021-03-09 05:58:06 -0500

    Change userId

--- src/main/java/edu/escuelaing/taskplanner/business/Entities/User.java
+++ src/main/java/edu/escuelaing/taskplanner/business/Entities/User.java
@@ -2,9 +2,14 @@ package edu.escuelaing.taskplanner.business.Entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Getter @Setter
+    @Id
     private String userId;
     @Getter @Setter
     private String userName;
